82c97c6d1c688fb20305dfcbbdf4c45af7244d1a
bioeggs <onurbomb1@gmail.com>
2026-01-19 08:59:42 +0100

    Create MorphModule.lua

---
+++ Modules/MorphModule.lua
@@ -0,0 +1,55 @@
+--[[
+
+MorphModule
+Script by: BioEggsHD (BallsHD)
+
+Handles morphing for players.
+
+]]
+
+--// PRINT OVERWRITE FOR LOGS //--
+
+local __print = print
+print = function(...)
+	if game:GetService("ServerScriptService").Modules:GetAttribute("ModulesDebug") == true then
+		__print("- " .. script.Name .. ": " .. ... .. ".")
+	else
+		return
+	end
+end
+
+--// VARIABLES & SERVICES //--
+
+local Morphs = script.Morphs
+
+--// MODULE //--
+
+local MorphModule = {}
+
+MorphModule.MorphPlayer = function(Player: Player, MorphName: string)
+	local MorphTemplate = Morphs[MorphName]
+	if not MorphTemplate then
+		print("ERROR: No morph \"" .. MorphName .. "\"")
+	end
+	
+	local Character = Player.Character or Player.CharacterAdded:Wait()
+	local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+	local OldCFrame = HumanoidRootPart.CFrame
+	
+	local Morph = MorphTemplate:Clone()
+	Morph.Name = Player.Name
+	Morph.Parent = workspace
+	
+	local MorphHRP = Morph:FindFirstChild("HumanoidRootPart")
+	if not MorphHRP then
+		print("ERROR: Morph has no HumanoidRootPart")
+		Morph:Destroy()
+		return
+	end
+	MorphHRP.CFrame = OldCFrame
+	
+	Player.Character = Morph
+	Character:Destroy()
+end
+
+return MorphModule
