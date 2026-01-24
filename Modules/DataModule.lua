9def45d5be61dcd523ad2880ca09ff0530e6cf45
bioeggs <onurbomb1@gmail.com>
2026-01-24 13:13:18 +0100

    Update DataModule.lua

--- Modules/DataModule.lua
+++ Modules/DataModule.lua
@@ -35,24 +35,53 @@ local DataModule = {}
 DataModule.LoadData = function(Player: Player)
 	local Name = Player.Name
 	local Uid = Player.UserId
-	local Data = DataStore:GetAsync(Uid) or DefaultData
-	for stat, value in DefaultData do
-		if not Data[stat] then
+
+	local Data = nil
+	local Retry = 3
+
+	while Retry > 0 do
+		local ok, result = pcall(function()
+			return DataStore:GetAsync(Uid)
+		end)
+		if ok then
+			Data = result
+			break
+		else
+			Retry -= 1
+			task.wait(1)
+		end
+	end
+
+	if not Data then
+		Data = table.clone(DefaultData)
+	end
+
+	for stat, value in pairs(DefaultData) do
+		if Data[stat] == nil then
 			Data[stat] = value
 		end
 	end
-	local leaderstats = Instance.new("Folder", Player)
+
+	local leaderstats = Instance.new("Folder")
 	leaderstats.Name = "leaderstats"
-	for statName, value in Data do
-		local ValueObject = Instance.new("NumberValue", leaderstats)
+	leaderstats.Parent = Player
+
+	for statName, value in pairs(Data) do
+		local ValueObject = Instance.new("NumberValue")
 		ValueObject.Name = statName
 		ValueObject.Value = value
+		ValueObject.Parent = leaderstats
 	end
+
 	local dataString = ""
-	for stat, value in Data do
-		dataString ..= stat .. ": " .. value .. ", "
+	for stat, value in pairs(Data) do
+		dataString ..= stat .. ": " .. tostring(value) .. ", "
 	end
-	dataString = dataString:sub(1, -3)
+
+	if #dataString > 2 then
+		dataString = dataString:sub(1, -3)
+	end
+
 	print("Data for player \"" .. Name .. "\": " .. dataString)
 end
 script.Bindable.LoadData.Event:Connect(DataModule.LoadData)
@@ -72,6 +101,10 @@ script.Bindable.SaveData.Event:Connect(DataModule.SaveData)
 
 DataModule.WipeData = function(Player: Player)
 	local Uid = Player.UserId
+	for _, value in Player.leaderstats:GetChildren() do
+		value.Value = DefaultData[value.Name]
+	end
+	DataStore:SetAsync(Uid, DefaultData)
 end
 script.Bindable.WipeData.Event:Connect(DataModule.WipeData)
 
