368108ecc03f95bdda09dc81bfd58018763d502a
bioeggs <onurbomb1@gmail.com>
2026-02-04 13:30:05 +0100

    Update ServerMain.lua

--- ServerMain.lua
+++ ServerMain.lua
@@ -7,17 +7,6 @@ Main framework & event connection manager.
 
 ]]
 
---// PRINT OVERWRITE FOR LOGS //--
-
-local __print = print
-print = function(...)
-	if game:GetService("ServerScriptService"):GetAttribute("DoDebug") == true then
-		__print(script.Name .. ": " .. ... .. ".")
-	else
-		return
-	end
-end
-
 --// VARIABLES & SERVICES //--
 
 local Players = game:GetService("Players")
@@ -25,86 +14,131 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
 local Teams = game:GetService("Teams")
 
-local Modules = ServerScriptService.Modules
-local AdminModule = require(Modules.AdminModule)
-local BadgeModule = require(Modules.BadgeModule)
-local BanModule = require(Modules.BanModule)
-local DataModule = require(Modules.DataModule)
-local GamepassModule = require(Modules.GamepassModule)
-local KillerModule = require(Modules.KillerModule)
-local MessageModule = require(Modules.MessageModule)
-local MapModule = require(Modules.MapModule)
-local MorphModule = require(Modules.MorphModule)
-local TeamModule = require(Modules.TeamModule)
-local TimerModule = require(Modules.TimerModule)
+local AdminModule = require(script.AdminModule)
+local BadgeModule = require(script.BadgeModule)
+local BanModule = require(script.BanModule)
+local DataModule = require(script.DataModule)
+local GamepassModule = require(script.GamepassModule)
+local KillerModule = require(script.KillerModule)
+local LeaderstatsModule = require(script.LeaderstatsModule)
+local MessageModule = require(script.MessageModule)
+local MapModule = require(script.MapModule)
+local MorphModule = require(script.MorphModule)
+local TeamModule = require(script.TeamModule)
+local TimerModule = require(script.TimerModule)
 
 --// FUNCTIONS //--
 
-function PlayerAdded(Player: Player)
+local function PlayerAdded(Player: Player)
 	print("Player Added: " .. Player.Name)
-	
+
 	AdminModule.CheckAdmin(Player)
 	BanModule.CheckBanned(Player)
-	DataModule.LoadData(Player)
+	local Data = DataModule.LoadData(Player)
+	LeaderstatsModule.InitPlayer(Player, Data)
 	GamepassModule.CheckVIP(Player)
 	BadgeModule.AwardMetOwner()
 end
-function PlayerRemoving(Player: Player)
+
+local function PlayerRemoving(Player: Player)
 	print("Player Removing: " .. Player.Name)
-	
+
 	DataModule.SaveData(Player)
+	
+	LeaderstatsModule.RemovePlayer(Player)
 end
 
-function GameLoop()
-	TimerModule.StartTimer(15)
-	MessageModule.Broadcast("Intermission")
-	task.wait(16)
-	
-	local PlayerCount = #Players:GetPlayers()
-	if PlayerCount >= 1 then -- change to > later	
+local function GameLoop()
+	while true do 
+		MessageModule.Broadcast("Intermission", 14)
 		TimerModule.StartTimer(15)
-		local Map = MapModule.PickRandomMap()
-		MessageModule.Broadcast("Map selected: " .. Map.Map.Name, 3)
-		task.wait(4)
-		
-		local Killer = KillerModule.PickRandomKiller()
-		MessageModule.Broadcast("Killer selected: " .. Killer.Name, 3)
-		task.wait(6)
-		
-		local Survivors = {}
-		for _, Player in Players:GetPlayers() do
-			if Player ~= Killer then
-				table.insert(Survivors, Player)
+		task.wait(15)
+
+		local AllPlayers = Players:GetPlayers()
+		local PlayerCount = #AllPlayers
+
+		if PlayerCount >= 2 then 
+			TimerModule.StartTimer(10)
+			
+			local Map = MapModule.PickRandomMap()
+			MessageModule.Broadcast("Map selected: " .. Map.Map.Name, 4)
+			task.wait(4)
+
+			local Killer = KillerModule.PickRandomKiller()
+
+			if not Killer or not Killer.Parent then
+				MessageModule.Broadcast("Killer left during selection. Restarting.", 3)
+				task.wait(3)
+				continue
 			end
-		end
-		TeamModule.AssignTeam({Killer}, Teams.Killer)
-		TeamModule.AssignTeam(Survivors, Teams.Survivors)
-		task.wait(6)
-		
-		TimerModule.StartTimer(15)
-		MapModule.TeleportToMap(Survivors, Map)
-		MessageModule.Broadcast("Hide from the killer!", 5)
-		task.wait(16)
-		
-		TimerModule.StartTimer(15)
-		MapModule.TeleportToMap({Killer}, Map)
-		MessageModule.Broadcast("Survive.", 5)
-		
-		while task.wait(5) do
-			if  TeamModule.GetTeamMemberCount(Teams.Survivors) < 1 or ReplicatedStorage.Timer.Value == 0 then
-				MessageModule.Broadcast("Round ended.", 5)
-				TeamModule.AssignTeam(Players:GetPlayers(), Teams.Lobby)
-				MapModule.TeleportToLobby(Players:GetPlayers())
-				break
+
+			MessageModule.Broadcast("Killer selected: " .. Killer.Name, 4)
+			task.wait(4)
+
+			local Survivors = {}
+			for _, Player in ipairs(Players:GetPlayers()) do
+				if Player ~= Killer then
+					table.insert(Survivors, Player)
+				end
 			end
+
+			if #Survivors == 0 then
+				MessageModule.Broadcast("Not enough survivors. Restarting.", 3)
+				task.wait(3)
+				continue
+			end
+
+			TeamModule.AssignTeam({Killer}, Teams.Killer)
+			TeamModule.AssignTeam(Survivors, Teams.Survivors)
+
+			MessageModule.Broadcast("Starting round.", 3)
+			task.wait(3)
+
+			MapModule.TeleportToMap(Survivors, Map)
+			MessageModule.Broadcast("Hide from the killer!", 15)
+			TimerModule.StartTimer(15)
+			task.wait(15)
+
+			if Killer and Killer.Parent then
+				MapModule.TeleportToMap({Killer}, Map)
+				MapModule.PlayAmbience(Map)
+				MessageModule.Broadcast("Survive.", 5)
+			end
+
+			local RoundTime = 120
+			TimerModule.StartTimer(RoundTime)
+
+			local RoundActive = true
+			local StartTime = os.clock()
+
+			while RoundActive do
+				task.wait(1)
+
+				local survivorCount = TeamModule.GetTeamMemberCount(Teams.Survivors)
+				local killerStillIn = (Killer.Parent == Players) 
+				local timeElapsed = os.clock() - StartTime
+
+				if survivorCount < 1 then
+					MessageModule.Broadcast("Killer Wins!", 5)
+					RoundActive = false
+				elseif not killerStillIn then
+					MessageModule.Broadcast("Killer left! Survivors Win!", 5)
+					RoundActive = false
+				elseif timeElapsed >= RoundTime then
+					MessageModule.Broadcast("Time Up! Survivors Win!", 5)
+					RoundActive = false
+				end
+			end
+
+			MapModule.StopAmbience(Map)
+			TeamModule.AssignTeam(Players:GetPlayers(), Teams.Lobby)
+			MapModule.TeleportToLobby(Players:GetPlayers())
+			task.wait(3)
+		else
+			MessageModule.Broadcast("Waiting for more players (2 required).", 5)
+			TimerModule.StartTimer(10)
+			task.wait(10)
 		end
-		
-		GameLoop()
-	else
-		MessageModule.Broadcast("Waiting for more players.", 6)
-		TimerModule.StartTimer(15, function()
-			GameLoop()
-		end)
 	end
 end
 
@@ -116,4 +150,4 @@ Players.PlayerRemoving:Connect(PlayerRemoving)
 --// MAIN //--
 
 task.wait(2)
-GameLoop()
+task.spawn(GameLoop)
