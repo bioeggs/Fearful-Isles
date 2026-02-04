932a1f3da41f8aaa1cb0b836e6fb7abe50ec6390
bioeggs <onurbomb1@gmail.com>
2026-02-04 13:36:33 +0100

    Update AdminModule.lua

--- Modules/AdminModule.lua
+++ Modules/AdminModule.lua
@@ -1,4 +1,4 @@
- --[[
+--[[
 
 AdminModule
 Script by: BioEggsHD (BallsHD)
@@ -8,31 +8,22 @@ If yes, give them admin permissions and a UI.
 
 ]]
 
---// PRINT OVERWRITE FOR LOGS //--
-
-local __print = print
-print = function(...)
-	if game:GetService("ServerScriptService").Modules:GetAttribute("ModulesDebug") == true then
-		__print("- " .. script.Name .. ": " .. ... .. ".")
-	else
-		return
-	end
-end
-
 --// MODULE //--
 
 local Whitelist = {
-	"Aqwertz333",
-	"MazinGotNewUser",
-	"lolololololgamer593",
-	"Not_RealDarkDreamer"
+	[1210462714] = true, -- Balls
+	[3409824679] = true, -- Balls alt
+	[2645344530] = true, -- Mazin
+	[1827342603] = true, -- Dusty
+	[2862614403] = true, -- Placeholder
 }
 
 local AdminModule = {}
 
 AdminModule.CheckAdmin = function(Player: Player)
 	local Name = Player.Name
-	if table.find(Whitelist, Name) then
+	local Uid = Player.UserId
+	if Whitelist[Uid] == true then
 		print("Player \"" .. Name .. "\" is admin, adding AdminUI")
 		script.AdminUI:Clone().Parent = Player.PlayerGui
 		Player:SetAttribute("Admin", true)
@@ -41,11 +32,9 @@ AdminModule.CheckAdmin = function(Player: Player)
 		Player:SetAttribute("Admin", false)
 	end
 end
-script.Bindable.CheckAdmin.Event:Connect(AdminModule.CheckAdmin)
 
 AdminModule.GetAdminNames = function()
 	return Whitelist
 end
-script.Bindable.GetAdminNames.OnInvoke = AdminModule.GetAdminNames
 
 return AdminModule
