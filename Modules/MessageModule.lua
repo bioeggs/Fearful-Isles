1a3cdac5643868f74e65e5cdbb207b97fc2763a4
bioeggs <onurbomb1@gmail.com>
2026-02-05 08:51:35 +0100

    Update MessageModule.lua

--- Modules/MessageModule.lua
+++ Modules/MessageModule.lua
@@ -4,28 +4,40 @@ MessageModule
 Script by: BioEggsHD (BallsHD)
 
 Handles messages as popups on the users screen.
-Can be used for notifications or updates.
 
 ]]
-
---// PRINT OVERWRITE FOR LOGS //--
-
-local __print = print
-print = function(...)
-	if game:GetService("ServerScriptService").Modules:GetAttribute("ModulesDebug") == true then
-		__print("- " .. script.Name .. ": " .. ... .. ".")
-	else
-		return
-	end
-end
 
 --// VARIABLES & SERVICES //--
 
 local Players = game:GetService("Players")
-local MessagingService = game:GetService("MessagingService")
 local TweenService = game:GetService("TweenService")
 
 local TweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut)
+
+--// FUNCTIONS //--
+
+local function createMessage(Player: Player, message: string)
+	local PlayerGui = Player:FindFirstChild("PlayerGui")
+	if not PlayerGui then return nil end
+
+	local UI = PlayerGui:FindFirstChild("MessageUI")
+	if not UI then return nil end
+
+	local NewMessage = script.Message:Clone()
+	NewMessage.Text = message
+	NewMessage.Parent = UI:FindFirstChild("MessagesContainer")
+
+	return NewMessage
+end
+
+local function hideMessage(message: TextLabel)
+	if not message then return end
+	local tween = TweenService:Create(message, TweenInfo, {TextTransparency = 1})
+	tween:Play()
+	tween.Completed:Connect(function()
+		message:Destroy()
+	end)
+end
 
 --// MODULE //--
 
@@ -36,21 +48,16 @@ MessageModule.Broadcast = function(message: string, duration: number)
 		MessageModule.ShowMessage(Player, message, duration)
 	end
 end
-script.Bindable.Broadcast.Event:Connect(MessageModule.Broadcast)
 
 MessageModule.ShowMessage = function(Player: Player, message: string, duration: number)
-	local NewMessage = script.Message:Clone()
-	NewMessage.Text = message
-	NewMessage.Parent = Player:WaitForChild("PlayerGui"):WaitForChild("MessageUI"):WaitForChild("MessagesContainer")
-	
-	TweenService:Create(NewMessage, TweenInfo, {TextTransparency = 0}):Play()
-	task.delay(duration, function()
-		TweenService:Create(NewMessage, TweenInfo, {TextTransparency = 1}):Play()
-		task.delay(0.3, function()
-			NewMessage:Destroy()
+	local NewMessage = createMessage(Player, message)
+	if NewMessage then
+		TweenService:Create(NewMessage, TweenInfo, {TextTransparency = 0}):Play()
+
+		task.delay(duration, function()
+			hideMessage(NewMessage)
 		end)
-	end)
+	end
 end
-script.Bindable.ShowMessage.Event:Connect(MessageModule.ShowMessage)
 
 return MessageModule
