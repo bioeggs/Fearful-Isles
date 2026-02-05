8d7477ebf75a5293c696909b968b9496aaf0f398
bioeggs <onurbomb1@gmail.com>
2026-02-05 09:34:54 +0100

    Update MorphModule.lua

--- Modules/MorphModule.lua
+++ Modules/MorphModule.lua
@@ -16,14 +16,18 @@ local Morphs = script.Morphs
 local MorphModule = {}
 
 MorphModule.MorphPlayer = function(Player: Player, MorphName: string)
-	local MorphTemplate = Morphs[MorphName]
+	local MorphTemplate = Morphs:FindFirstChild(MorphName)
 	if not MorphTemplate then
-		print("ERROR: No morph \"" .. MorphName .. "\"")
+		warn("ERROR: No morph \"" .. MorphName .. "\"")
+		return
 	end
 	
 	local Character = Player.Character or Player.CharacterAdded:Wait()
 	local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 	local OldCFrame = HumanoidRootPart.CFrame
+	
+	-- Store original character for morph-back
+	Character.Parent = nil
 	
 	local Morph = MorphTemplate:Clone()
 	Morph.Name = Player.Name
@@ -31,14 +35,53 @@ MorphModule.MorphPlayer = function(Player: Player, MorphName: string)
 	
 	local MorphHRP = Morph:FindFirstChild("HumanoidRootPart")
 	if not MorphHRP then
-		print("ERROR: Morph has no HumanoidRootPart")
+		warn("ERROR: Morph has no HumanoidRootPart")
 		Morph:Destroy()
+		Character.Parent = workspace
 		return
 	end
+	
 	MorphHRP.CFrame = OldCFrame
 	
 	Player.Character = Morph
-	Character:Destroy()
+end
+
+MorphModule.MorphBack = function(Player: Player, OldCharacter: Model, OldMorph: Model)
+	if not OldCharacter then 
+		warn("ERROR: No OldCharacter")
+		return
+	end
+	if not OldMorph then 
+		warn("ERROR: No OldMorph")
+		return
+	end
+	
+	if not OldMorph.Parent then
+		warn("ERROR: OldMorph no longer exists")
+		return
+	end
+	
+	local MorphHRP = OldMorph:FindFirstChild("HumanoidRootPart")
+	if not MorphHRP then
+		warn("ERROR: Morph has no HumanoidRootPart")
+		return
+	end
+	
+	local OldCFrame = MorphHRP.CFrame
+	
+	OldCharacter.Parent = workspace
+	
+	local CharacterHRP = OldCharacter:FindFirstChild("HumanoidRootPart")
+	if not CharacterHRP then
+		warn("ERROR: Character has no HumanoidRootPart")
+		OldCharacter:Destroy()
+		return
+	end
+	
+	CharacterHRP.CFrame = OldCFrame
+	
+	Player.Character = OldCharacter
+	OldMorph:Destroy()
 end
 
 return MorphModule
